bb673e5161aab9edd8e9aaa6063d066651aad5c6
Yeti <106940748+Os51@users.noreply.github.com>
2024-02-01 00:53:32 +0000

    Create comparing.py

---
+++ conditionals/comparing.py
@@ -0,0 +1,4 @@
+# Python can be used to compare values to each other.
+# We use Operators to perform comparisons, and can compare Equations or Strings. We can look to see if something is bigger than, smaller than or equal to another value
+# A comparison operation results in a boolean output of True or False
+# Other than equality, you can't do a comparison between different data types, like a string and int
