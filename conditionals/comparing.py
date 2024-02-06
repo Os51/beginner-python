8212fb62ca08dda4fa9eacb73c9a1d5e80dcc544
Yeti <106940748+Os51@users.noreply.github.com>
2024-02-06 14:37:48 +0000

    Update comparing.py

--- conditionals/comparing.py
+++ conditionals/comparing.py
@@ -2,3 +2,9 @@
 # We use Operators to perform comparisons, and can compare Equations or Strings. We can look to see if something is bigger than, smaller than or equal to another value
 # A comparison operation results in a boolean output of True or False
 # Other than equality, you can't do a comparison between different data types, like a string and int
+
+print("cat" == "cat")
+print(4 > 2)
+print(2**2 == 4)
+# print (2 > "2") - results in an error, but the below is valid:
+print("2" == 2)
